b0b77d4ee86fb5fd418fda2298335fe28d976eda
Tarik Besic <tarik_besic@live.com>
2023-01-15 00:33:03 +0100

    added airbnb eslint rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   content: [
-    './pages/**/*.{html,ts,tsx,js,jsx}',
-    './components/**/*.{html,ts,tsx,js,jsx}',
+    "./pages/**/*.{html,ts,tsx,js,jsx}",
+    "./components/**/*.{html,ts,tsx,js,jsx}",
   ],
+  theme: {
+    "text-input": "#8c8c8c",
+  },
   // ...
-}
+};
