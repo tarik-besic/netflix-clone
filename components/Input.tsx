da1aa83c371e358156a6516d4197ed86106d1cd1
Tarik Besic <tarik_besic@live.com>
2023-01-15 19:55:18 +0100

    show and hide password for input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,4 +1,4 @@
-import { ErrorMessage, Field, FieldInputProps, useField } from "formik";
+import { FieldInputProps, useField } from "formik";
 import { useRef, useState } from "react";
 
 // interface for inputFiled that extends default props used in Formik forms
@@ -13,26 +13,42 @@ const InputField = (props: Props) => {
   // props
   const { label, className, name } = props;
   const inputRef = useRef<HTMLInputElement>(null);
-  const [active, setActive] = useState(false);
-  const [field, meta, helper] = useField(name);
+  const [field] = useField(name);
 
+  // states
+  const [active, setActive] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  // boolean value for floating label
   const transformInput = (!active && field.value) || active;
 
   return (
     <div
-      className={`h-[50px] relative flex flex-col outline-none bg-neutral-700 rounded-t ${className} ${
-        !transformInput ? "cursor-pointer" : ""
-      }"}`}
+      className={`h-[50px] cursor-text relative flex flex-col outline-none rounded-t rounded-b ${className} ${
+        !transformInput ? "bg-[#454545]" : ""
+      } ${active ? "bg-[#454545]" : "bg-[#333333]"}`}
+      onClick={() => inputRef.current!.focus()}
     >
-      <input
-        ref={inputRef}
-        onFocus={() => setActive(true)}
-        className="h-[50px] pt-4 pb-0 px-5 bg-transparent border-transparent focus:border-transparent focus:outline-none"
-        {...field}
-        onBlur={() => setActive(false)}
-      />
+      <div className="flex items-center">
+        <input
+          {...field}
+          ref={inputRef}
+          onFocus={() => setActive(true)}
+          className="text-white h-[50px] pt-4 pb-0 px-5 bg-transparent border-transparent focus:border-transparent focus:outline-none"
+          type={showPassword ? "text" : props.type}
+          onBlur={() => setActive(false)}
+        />
+        {props.type === "password" && (
+          <div
+            className="text-[#8c8c8c] mr-3 cursor-pointer"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? "SHOW" : "HIDE"}
+          </div>
+        )}
+      </div>
       <label
-        onClick={(e) => {
+        onClick={() => {
           setActive(true);
           inputRef.current!.focus();
         }}
