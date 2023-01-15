d18eca61abe5c4290ae0253150081b5deac3e7d2
Tarik Besic <tarik_besic@live.com>
2023-01-15 19:56:12 +0100

    formatted by eslint rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import LoginForm from "../components/LoginForm";
 
-const Home: NextPage = () => {
-  return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      <div className="bg-black absolute w-screen h-screen -z-20 overflow-hidden">
-        <img className="opacity-50" src="/images/background-netflix.webp" />
-      </div>
-      <LoginForm />
+const Home: NextPage = () => (
+  <div className="flex items-center justify-center w-screen h-screen">
+    <div className="bg-black absolute w-screen h-screen -z-20 overflow-hidden">
+      <Image className="opacity-50" src="/images/background-netflix.webp" alt="netflix background" fill/>
     </div>
-  );
-};
+    <LoginForm />
+  </div>
+);
 
 export default Home;
