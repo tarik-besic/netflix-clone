b15f12c96ed9c3c5a92fc01550e0f1d80bafedeb
Tarik Besic <tarik_besic@live.com>
2023-01-28 19:55:24 +0100

    footer style fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className="absolute bottom-0 left-0 bg-black opacity-80< flex items-center justify-center w-full mt-auto h-24 border-t">
+  <footer className="absolute bottom-0 left-0 bg-black opacity-80 flex items-center justify-center w-full mt-auto h-24 border-t">
     <div>Footer</div>
   </footer>
 );
